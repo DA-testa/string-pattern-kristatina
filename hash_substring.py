3494b12d21e4c9e7cd134b487fbcc7dce0bb6e23
Kristīne Berķe 221RDB109 <125013804+kristatina@users.noreply.github.com>
2023-04-02 09:24:07 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -24,24 +24,27 @@ def get_occurrences(pattern, text):
     # and return an iterable variable
     p = len(pattern)
     t = len(text)
-    
+
+   
     pattern_hash = sum(ord(pattern[i]) * pow(10, p-i-1) for i in range(p))
-    text_hash = sum(ord(text[i]) * pow(10,p-i-1) for i in range(p))
+    text_hash = sum(ord(text[i]) * pow(10, p-i-1) for i in range(p))
     result = []
-    
+ 
     for i in range(t-p+1):
+        # Check the hash values of current window of text and pattern
         if text_hash == pattern_hash:
+            # Check for characters one by one
             match = True
             for j in range(p):
                 if text[i+j] != pattern[j]:
                     match = False
-                   break
+                    break
             if match:
                 result.append(i)
 
         if i < t-p:
-            text_hash = (text_hash - ord(text[i])*pow(10,p-1))*10 + ord(text[i+p])
-
+            text_hash = (text_hash - ord(text[i]) * pow(10, p-1)) * 10 + ord(text[i+p])
+ 
     return result
 
 # this part launches the functions
